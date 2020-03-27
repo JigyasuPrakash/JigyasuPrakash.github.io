0585bf5a96b2314fc1fc4e09c6010c65d358c0be
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-03-27 19:54:46 +0530

    remove old stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# itsjigyasu.me
-
-This repository contains content for my <a href="https://itsjigyasu.me">website</a>
--- js/index.js
+++
@@ -1,2 +0,0 @@
-var lastUpdate = document.getElementById('last-update');
-lastUpdate.textContent = 'Last Updated: 16 March, 2020';
