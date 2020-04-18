c8e21551429aeb6457e1bee1d6705ce0e07efa42
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-04-18 16:17:41 +0530

    react particle fixed

--- public/index.js
+++ public/index.js
@@ -1,9 +1,5 @@
 window.onscroll = () => {
-    const nav = document.querySelector('#thisisnav');
-    if (this.scrollY <= 5) nav.className = '';
-    else nav.className = 'mynav';
+  const nav = document.querySelector("#thisisnav");
+  if (this.scrollY <= 5) nav.className = "";
+  else nav.className = "mynav";
 };
-
-particlesJS.load("particles-js", "particles.json", function () {
-    console.log("particles.json loaded..");
-});
--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
-import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
-import './App.css';
-import NotFoundPage from './component/NotFoundPage';
-import 'w3-css';
-import Header from './component/Header';
-import More from './component/More';
-import Projects from './component/Projects';
-import Home from './component/Home';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React, { Component } from "react";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import "./App.css";
+import NotFoundPage from "./component/NotFoundPage";
+import "w3-css";
+import Header from "./component/Header";
+import More from "./component/More";
+import Projects from "./component/Projects";
+import Home from "./component/Home";
+import Particle from "./component/Particle";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Header />
-        <Route render={({ location }) => (
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              timeout={450}
-              classNames="fade">
-              <Switch location={location}>
-                <Route exact path="/" component={Home} />
-                <Route path="/projects" component={Projects} />
-                <Route path="/more" component={More} />
-                <Route path="/404" component={NotFoundPage} />
-                <Redirect to="/404" />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )} />
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={450} classNames="fade">
+                <Switch location={location}>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/projects" component={Projects} />
+                  <Route path="/more" component={More} />
+                  <Route path="/404" component={NotFoundPage} />
+                  <Redirect to="/404" />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
+        <Particle />
       </BrowserRouter>
-    )
+    );
   }
 }
 
