84acc485ae6bbbc62844859acf4f0d2872640e2e
Jigyasu Prakash <jigyasuprakash3@gmail.com>
2022-02-20 20:29:48 +0530

    rebranding to github.io

---
+++ build/precache-manifest.156f0411bad72a57a81c5bbd5b86df58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5701fac2d74d52895983be366bf6f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35e64571ddc573ec39b",
+    "url": "/static/css/2.b1673db3.chunk.css"
+  },
+  {
+    "revision": "385987921a16229c3c12",
+    "url": "/static/css/main.080c7f76.chunk.css"
+  },
+  {
+    "revision": "c35e64571ddc573ec39b",
+    "url": "/static/js/2.9da5dafe.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.9da5dafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385987921a16229c3c12",
+    "url": "/static/js/main.bf55c9c3.chunk.js"
+  },
+  {
+    "revision": "8786cd60317b02073ca4",
+    "url": "/static/js/runtime-main.ee58f54d.js"
+  }
+]);
